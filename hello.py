e20a1225df43b56658e6892cad8b251fda3782ff
4RUN555 <68030316@kmitl.ac.th>
2025-08-06 15:26:15 +0700

    d

--- hello.py
+++ hello.py
@@ -3,9 +3,10 @@ import config
 
 def say_hello(name):
     print(f"Hello,{name} from {config.APP_NAME}!")
+    print(f"Today is {now.strftime('%Y-%m-%d')}.") 
 def greet_user():
     name = input("Please enter your name: ")
     say_hello(name)
 if __name__ == "__main__":
     greet_user()
-    #แมวfgg
+    #แมว
