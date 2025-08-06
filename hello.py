32c370fd88933727e4bfbaab707a9013d9e4c847
4RUN555 <68030316@kmitl.ac.th>
2025-08-06 15:28:24 +0700

    a

--- hello.py
+++ hello.py
@@ -2,6 +2,7 @@ import datetime
 import config
 
 def say_hello(name):
+    now = datetime.datetime.now() 
     print(f"Hello,{name} from {config.APP_NAME}!")
     print(f"Today is {now.strftime('%Y-%m-%d')}.") 
 def greet_user():
